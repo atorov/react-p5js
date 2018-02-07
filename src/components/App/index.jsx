2aa47c926a34f78cacb343e753f6a2a4c1ef7b34
Veselin <veselin.lk.petrov@gmail.com>
2018-02-07 18:49:39 +0200

    styling improvements

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -21,7 +21,7 @@ export default class App extends Component {
           <input type="range"
             min={5} max={250} step={1}
             value={this.state.slider}
-            style={{ width: "90%" }}
+            style={{ width: "90%", maxWidth: "900px" }}
             onChange={this.onSliderChange}
           />
         </div>
