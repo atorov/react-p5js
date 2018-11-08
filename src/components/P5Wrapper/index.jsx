6f15297490a2a11ab417278e914d9ce47151ebdd
Veselin <veselin.lk.petrov@gmail.com>
2018-11-08 17:41:59 +0200

    added multiple independent sketches support and an example how to split the sketch code into separate files

--- src/components/P5Wrapper/index.jsx
+++ src/components/P5Wrapper/index.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import sketch1 from './sketch1'
+import sketch2 from './sketch2'
 
 export default class P5Wrapper extends Component {
     static propTypes = {
@@ -13,23 +14,34 @@ export default class P5Wrapper extends Component {
         this.canvas1 = new window.p5(sketch1, 'canvas1-container')
         this.canvas1.props = this.props.p5Props
         this.canvas1.onSetAppState = this.props.onSetAppState
+
+        this.canvas2 = new window.p5(sketch2, 'canvas2-container')
+        this.canvas2.props = this.props.p5Props
     }
 
     shouldComponentUpdate(nextProps) {
         this.canvas1.props = nextProps.p5Props
+        this.canvas2.props = nextProps.p5Props
         return false
     }
 
     componentWillUnmount() {
         this.canvas1.remove()
+        this.canvas2.remove()
     }
 
     render() {
         return (
-            <div
-                id="canvas1-container"
-                style={{ width: "100%", textAlign: "center" }}
-            />
+            <>
+                <div
+                    id="canvas2-container"
+                    style={{ width: "100%", textAlign: "center" }}
+                />
+                <div
+                    id="canvas1-container"
+                    style={{ width: "100%", textAlign: "center" }}
+                />
+            </>
         )
     }
 }
--- src/components/P5Wrapper/sketch2/lib.js
+++ src/components/P5Wrapper/sketch2/lib.js
@@ -1,6 +1,6 @@
 export function drawBezier(s, x1, y1, x2, y2) {
     s.noStroke()
-    s.fill(127, 255, 205, 63)
+    s.fill(127, 0, 50, 31)
     s.bezier(
         x1, y1,
         s.width / 2 - s.props.slider * 2, s.height / 2 - s.props.slider,
@@ -9,21 +9,12 @@ export function drawBezier(s, x1, y1, x2, y2) {
     )
 }
 
-export function drawFloatingText(s, rx, ry, label, value) {
-    s.textAlign(s.CENTER);
+export function drawFloatingText(s, rx, ry) {
     s.noStroke()
-    s.fill(127, 255, 205, 191)
-    s.textSize(14)
-    s.text(`${label}: ${value}`, rx, ry - 8)
-
-    s.stroke(127, 255, 205)
-    s.strokeWeight(1)
-    s.fill(127, 255, 205, 191)
+    s.fill(127, 0, 50, 191)
     s.ellipse(rx, ry, 8, 8)
 
     s.textAlign(s.CENTER);
-    s.noStroke()
-    s.fill(127, 255, 205, 191)
     s.textSize(12)
     s.text(`(${s.round(rx)}, ${s.round(ry)})`, rx, ry + 16)
 }
